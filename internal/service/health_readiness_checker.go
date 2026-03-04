14b1f251bce25146fab52a54381b0f318342d5c1
egorgusakov <egusyakov@fmlogistic.com>
2026-03-04 23:33:40 +0300

    refactor(service): rename greeter service to health readiness checker
    - Renamed internal/service/greeter.go to health_readiness_checker.go
    - Updated HealthService methods to provide health and readiness endpoints
    - Added Ready method placeholder returning readiness

--- internal/service/health_readiness_checker.go
+++ internal/service/health_readiness_checker.go
@@ -25,11 +25,18 @@ func NewHealthService() *HealthService {
 
 // Health implements checking service viability
 func (s *HealthService) Health(context.Context, *emptypb.Empty) (*v1.HealthReply, error) {
+	return &v1.HealthReply{
+		GatewayStatus: fmt.Sprintf(variables.ServiceIsUp, "Gateway"),
+		GatewayUptime: s.uptime.Format("2006-" +
+			"01-02 15:04:05"),
+	}, nil
+}
+
+func (s *HealthService) Ready(context.Context, *emptypb.Empty) (*v1.ReadinessReply, error) {
 	/*
 		todo check services
 	*/
-	return &v1.HealthReply{
-		GatewayStatus: fmt.Sprintf(variables.ServiceIsUp, "Gateway"),
-		GatewayUptime: s.uptime.Format("2006-01-02 15:04:05"),
+	return &v1.ReadinessReply{
+		Status: "Тут пока ничего нет и ничего не проверяется ))))))",
 	}, nil
 }
